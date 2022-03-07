8e8d761aa82619b4bdec674d05ce00922e4a99f6
Erick Gustavo <erick.stazs@gmail.com>
2022-03-07 16:39:38 -0300

    Create screen component

--- main.js
+++ main.js
@@ -1,6 +1,28 @@
-import './style.css';
+import './src/styles/reset.css';
+import './src/styles/colors.css';
+import './src/styles/base.css';
+import './src/styles/calculator-app.css';
+import './src/styles/calc-screen.css';
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`;
+const $screen = getElement('screen');
+
+function getElement(elementName) {
+  return document.querySelector(`[data-js="${elementName}"]`);
+}
+
+function showCurrentOperation(value) {
+  const $current = document.createElement('div');
+  $current.className = 'current';
+  $current.textContent = value;
+  $screen.appendChild($current);
+}
+
+function showResultOperation(value) {
+  const $result = document.createElement('div');
+  $result.className = 'result';
+  $result.textContent = value;
+  $screen.appendChild($result);
+}
+
+showCurrentOperation('1 + 2');
+showResultOperation('3');
